d5c9891e757187db3e10955237361e7964dda608
yanghao <fexeak@163.com>
2025-12-03 09:35:13 +0800

    Initial LazyVim config

---
+++ lua/config/options.lua
@@ -0,0 +1,19 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+vim.g.autoformat = true
+
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+
+local opt = vim.opt
+
+opt.autowrite = true -- Enable auto write
+
+if not vim.env.SSH_TTY then
+  -- only set clipboard if not in ssh, to make sure the OSC 52
+  -- integration works automatically. Requires Neovim >= 0.10.0
+  opt.clipboard = "unnamedplus" -- Sync with system clipboard
+end
